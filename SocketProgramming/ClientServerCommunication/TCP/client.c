ea9d2bc566095e08deb4a226f3d1ba42136deba5
Ajil <39672981+Ajil5467@users.noreply.github.com>
2020-03-03 15:35:23 +0530

    update

--- SocketProgramming/ClientServerCommunication/TCP/client.c
+++ SocketProgramming/ClientServerCommunication/TCP/client.c
@@ -1,33 +1,62 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+void func(int sockfd) 
+{ 
+	char buff[MAX]; 
+	int n; 
+	for (;;) 
+	{ 
+		bzero(buff, sizeof(buff)); 
+		printf("Enter the string : "); 
+		n = 0; 
+		while ((buff[n++] = getchar()) != '\n') 
+			; 
+		write(sockfd, buff, sizeof(buff)); 
+		bzero(buff, sizeof(buff)); 
+		read(sockfd, buff, sizeof(buff)); 
+		printf("From Server : %s", buff); 
+		if ((strncmp(buff, "exit", 4)) == 0) 
+		{ 
+			printf("Client Exit...\n"); 
+			break; 
+		} 
+	} 
+} 
 
-#include <sys/types.h>
-#include <stdio.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-int main()
-{
-    int clientsocket;
-    struct sockaddr_in serveraddr;
-    socklen_t len;
-    struct hostent *server;
-    char message[50];
-    clientsocket = socket(AF_INET, SOCK_STREAM, 0);
-    bzero((char *)&serveraddr, sizeof(serveraddr));
-    len = sizeof(serveraddr);
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(5030);
-    server = gethostbyname("127.0.0.1");
-    bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, sizeof(server->h_addr));
-    printf("\n[C]: PRESS ENTER TO START THE CONNECTION PROCESS.\n");
-    fgets(message, 2, stdin);
-    connect(clientsocket, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-    printf("\n[C]: Sending message for server connection... \n");
-    write(clientsocket, "hi i am client", sizeof("hi i am client"));
-    printf("\n[C]: Message sent. \n");
-    printf("\n[C]: Receiving message from server...\n");
-    read(clientsocket, message, sizeof(message));
-    printf("\n[C]: Message recieved: %s", message);
-    close(clientsocket);
-}
+int main() 
+{ 
+	int sockfd, connfd; 
+	struct sockaddr_in servaddr, cli; 
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) 
+	{ 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_port = htons(PORT); 
+
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) 
+	{ 
+		printf("connection with the server failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("connected to the server..\n"); 
+
+	func(sockfd); 
+
+	close(sockfd); 
+} 
